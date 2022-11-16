b590da0800aeaa8da3eb769cdb1238f3374fb665
eoooomji <akcks101@gmail.com>
2022-11-16 21:48:59 +0900

    update | 20221116

---
+++ spring_03_aop/src/main/java/part01_xml/ServiceImp.java
@@ -0,0 +1,58 @@
+package part01_xml;
+
+import java.util.Random;
+
+// 핵심 로직만 구현해 놓은 클래스 - Target
+public class ServiceImp implements Service {
+	public ServiceImp() {
+
+	}
+
+	@Override
+	public void prn1() {
+		System.out.println("prn");
+
+	}
+
+	@Override
+	public void prn1(int x) {
+		System.out.println("x : " + x);
+
+	}
+
+	@Override
+	public void prn1(Random ran) {
+		System.out.println(ran.nextDouble());
+
+	}
+
+	@Override
+	public void prn1(int a, int b) {
+		System.out.printf("a=%d, b=%d\n", a, b);
+
+	}
+
+	@Override
+	public void prn2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String prn3() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void prn4() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void prn5() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ spring_03_aop/src/main/java/part01_xml/SpringTest.java
@@ -0,0 +1,34 @@
+package part01_xml;
+
+import java.util.Random;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/*
+ * <!-- https://mvnrepository.com/artifact/org.aspectj/aspectjweaver -->
+		<dependency>
+			<groupId>org.aspectj</groupId>
+			<artifactId>aspectjweaver</artifactId>
+			<version>1.9.7</version>
+			<scope>runtime</scope>
+		</dependency>
+	maven을 추가 시켜주면 된다.
+ */
+public class SpringTest {
+
+	public static void main(String[] args) {
+		String path = "part01_xml/aop.xml";
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(path);
+		Service svc = null;
+		svc = (Service) context.getBean("svc");
+		svc.prn1();
+		svc.prn1(10);
+		svc.prn1(new Random());
+		svc.prn1(20, 30);
+		
+
+	} // end main()
+
+} // end class
