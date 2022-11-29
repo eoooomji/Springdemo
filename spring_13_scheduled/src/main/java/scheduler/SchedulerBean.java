7739e4eb908ff76b1489eed02aa9b28d1d444083
eoooomji <akcks101@gmail.com>
2022-11-29 17:25:25 +0900

    update | 20221129

--- spring_13_scheduled/src/main/java/scheduler/SchedulerBean.java
+++ spring_13_scheduled/src/main/java/scheduler/SchedulerBean.java
@@ -16,7 +16,34 @@ public class SchedulerBean {
 		Calendar calendar = Calendar.getInstance();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		logger.info("스케쥴링 실행: " + dateFormat.format(calendar.getTime()));
-		System.out.println("스케쥴링 실행: " + dateFormat.format(calendar.getTime()));
+		/*
+		src/main/resources/log4j.xml
+		<!-- Root Logger -->
+		<root>
+			<priority value="info" />
+			<appender-ref ref="console" />
+		</root>
+		
+		로그(log) : 프로그램 개발이나 운영시 문제점을 추적하거나 운영상태를 모니터링하기 위한 기능
+		확인하는 방법 : System.out.println();을 사용하여 로그를 확인할 수 있으나 로그(log)를 기록하는 방법을 사용한다.
+		
+		log4j.xml은 다음과 같은 레벨을 갖는다.
+		TRACE > DEBUG > INFO > WARN > ERROR > FATAL
+		(만약에 INFO로 세팅하면 INFO, WRAN, ERROR, FATAL은 기록된다.)
+		
+		FATAL : 심각한 에러 (프로그램 자체가 실행되지 않을 수 있다.)
+		ERROR : 문제가 발생하면 소프트웨어 일부가 기능을 수행하지 못할 수 있다.
+		WARNING : 소프트웨어가 동작하고 있지만, 예상지 못한 일이 발생하거나 가까운 미래에 문제가 발생할 수 있다는 경고이다.
+				  ex) 디스크 공간 부족 -> 프로그램은 동작되지만 앞으로 문제가 발생할 수도 있음
+		INFO : 작업이 정상적으로 작동하고 있음을 메세지를 통해 확인하고 싶은 경우에 사용한다.
+		DEBUG : 어떤 문제가 발생했을 경우 문제를 진단하고 싶을 때 필요한 자세한 정보를 기록한다.
+				개발시 디버그 용도로 사용하는 매새자를 나타낸다.
+		TRACE : 디버그 레벨이 너무 광범위한 것을 해결하기 위해서 좀 더 상세한 이벤트를 나타낸다.
+		
+		*/
+		
+		//System.out.println("스케쥴링 실행: " + dateFormat.format(calendar.getTime()));
+		
 	} // end scheduleRun()
 }
 
